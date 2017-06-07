feeb49f1fb218344d974be485585ff1fb85902f2
Minghao Liu <molikto@gmail.com>
2017-06-07 22:48:56 +0800

    clicking working

--- core/src/org/snailya/bnw/BraveNewWorld.kt
+++ core/src/org/snailya/bnw/BraveNewWorld.kt
@@ -8,13 +8,17 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import ktx.scene2d.*
 import ktx.style.color
 import ktx.style.label
 import ktx.style.skin
+import ktx.style.textButton
 import org.snailya.base.*
 
 /**
@@ -40,6 +44,10 @@ class BraveNewWorld(pdi: PlatformDependentInfo) : ApplicationInner(pdi) {
             font = RototoMono14
             fontColor = Color.WHITE
         }
+        textButton {
+            font = RototoMono14
+            fontColor = Color.WHITE
+        }
     }
 
     init { Scene2DSkin.defaultSkin = defaultSkin }
@@ -51,15 +59,24 @@ class GamePage : Page() {
 
     val img = Texture("badlogic.jpg")
 
+    var pos = Vector2(0F, 0F)
+
     init {
         ui = table {
             label("DEBUG TEXT")
+            textButton("BUTTON") {
+                addListener(object : ClickListener() {
+                    override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                        pos = Vector2(x, y)
+                    }
+                })
+            }
         }
     }
 
     override fun render() {
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(img, pos.x, pos.y)
         batch.end()
     }
 
