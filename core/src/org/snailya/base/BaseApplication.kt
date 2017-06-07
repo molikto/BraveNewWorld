f89aaab6a18e1d7403e34fc790b746fa605a2129
Minghao Liu <molikto@gmail.com>
2017-06-07 18:41:11 +0800

    structural changes

--- core/src/org/snailya/base/BaseApplication.kt
+++ core/src/org/snailya/base/BaseApplication.kt
@@ -1,11 +1,8 @@
 package org.snailya.base
 
-import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.Screen
 
 
 /**
@@ -14,16 +11,27 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
  */
 class PlatformDependentInfo(val iOSScale: Float?, val logicalWidth: Int?)
 
-abstract class ApplicationInner(pdi: PlatformDependentInfo) {
+abstract class Page {
+    open fun resume() {}
+    open fun pause() {}
+    open fun dispose() {}
+    open fun render() {}
+    open fun resize(width: Int, height: Int) {}
+}
 
-    val dpiPixel: Float = pdi.iOSScale ?: (if (pdi.logicalWidth != null) (backBufferWidth().toFloat() / pdi.logicalWidth) else Gdx.graphics.density)
+abstract class ApplicationInner(pdi: PlatformDependentInfo) {
 
     fun backBufferWidth() = Gdx.graphics.backBufferWidth
     fun backBufferHeight() = Gdx.graphics.backBufferHeight
 
+    /**
+     * these are calculated ourselves, seems good to NOT use LIBGDX's API
+     */
+    val dpiPixel: Float = pdi.iOSScale ?: (if (pdi.logicalWidth != null) (backBufferWidth().toFloat() / pdi.logicalWidth) else Gdx.graphics.density)
     fun width(): Float = backBufferWidth() / dpiPixel
     fun height(): Float = backBufferHeight() / dpiPixel
 
+    // TODO logical size image loader
 
     init {
         debug("Pixel density: $dpiPixel," +
@@ -34,11 +42,17 @@ abstract class ApplicationInner(pdi: PlatformDependentInfo) {
 
 
 
-    fun resume() {}
-    fun pause() {}
-    open fun render() {}
-    open fun dispose() {}
-    fun resize(width: Int, height: Int) {}
+
+    lateinit var page: Page
+
+
+    // maybe good place to stop/start game logic??
+    open fun resume() = page.resume()
+    open fun pause() = page.pause()
+    open fun dispose() = page.dispose()
+    open fun render() = page.render()
+
+    fun resize(width: Int, height: Int) = page.resize(width, height)
 }
 
 open class ApplicationWrapper(val factory: () -> ApplicationInner) : ApplicationAdapter() {
--- core/src/org/snailya/bnw/BraveNewWorld.kt
+++ core/src/org/snailya/bnw/BraveNewWorld.kt
@@ -1,32 +1,49 @@
 package org.snailya.bnw
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import org.snailya.base.ApplicationInner
 import org.snailya.base.ApplicationWrapper
+import org.snailya.base.Page
 import org.snailya.base.PlatformDependentInfo
 
-class BraveNewWorldWrapper(pdi: PlatformDependentInfo) : ApplicationWrapper({ BraveNewWorldInner(pdi) })
+class BraveNewWorldWrapper(pdi: PlatformDependentInfo) : ApplicationWrapper({ BraveNewWorld(pdi) })
 
-class BraveNewWorldInner(pdi: PlatformDependentInfo) : ApplicationInner(pdi) {
+var _bnw: BraveNewWorld? = null
+val bnw by lazy { _bnw!! }
+
+class BraveNewWorld(pdi: PlatformDependentInfo) : ApplicationInner(pdi) {
 
     init {
+        _bnw = this
+        page = GamePage()
     }
+
     val batch = SpriteBatch()
+
+    override fun dispose() {
+        super.dispose()
+        batch.dispose()
+    }
+}
+
+class GamePage : Page() {
+
     val img = Texture("badlogic.jpg")
 
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        bnw.batch.begin()
+        bnw.batch.draw(img, 0f, 0f)
+        bnw.batch.end()
     }
 
     override fun dispose() {
-        batch.dispose()
         img.dispose()
-   }
+    }
 }
+
